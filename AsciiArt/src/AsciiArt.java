0539a0059c8d7896098b1629f833363d74545372
Noa Rogoszinski <nrogoszinski19@ssdsnassau.org>
2018-09-25 21:48:14 -0400

    Ascii Art assignment

--- AsciiArt/src/AsciiArt.java
+++ AsciiArt/src/AsciiArt.java
@@ -1,4 +1,25 @@
 
 public class AsciiArt {
-
+	public static void main(String args[]) {
+		System.out.println("         ,");
+		System.out.println("        /|      __");
+		System.out.println("       / |   ,-- /");
+		System.out.println("      Y :|  //  /");
+		System.out.println("      | :| //  ^");
+		System.out.println("      >-\"-\"-v\"/");
+		System.out.println("     /       Y");
+		System.out.println("    jo  o    |");
+		System.out.println("   ( ~T~     j");
+		System.out.println("    >._-' _./");
+		System.out.println("   /   \"-\"  |");
+		System.out.println("  Y     _,  |");
+		System.out.println(" /| ;-\"- _  |");
+		System.out.println("/ |/ ,-\"-    \\");
+		System.out.println("\\//\\/      .- \\");
+		System.out.println(" Y        /    Y");
+		System.out.println(" |       |     |");
+		System.out.println(" ]\\      _\\    /\"\\");
+		System.out.println("(\" -----(     Y.  )");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+	}
 }
--- AsciiArt/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author noarogoszinski
- *
- */
-module AsciiArt {
-}
