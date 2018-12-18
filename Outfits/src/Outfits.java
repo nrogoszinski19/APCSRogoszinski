730b070bf76f305406256c8797b698dc556840f4
Noa Rogoszinski <nrogoszinski19@ssdsnassau.org>
2018-12-18 15:27:58 -0500

    Outfits

---
+++ Outfits/src/Outfits.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.applet.*;
+
+
+public class Outfits extends Applet{
+	
+	public void paint(Graphics g) {
+		Person person = new Person(g, 70);
+		
+
+	}
+
+}
--- Outfits/src/Person.java
+++ Outfits/src/Person.java
@@ -1,9 +1,15 @@
+import java.awt.*;
 
 public class Person {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public Person(Graphics g, int x) {
+		g.setColor(Color.black);
+		g.fillOval(x, 100, 70, 70);
+		g.fillRect(x-5, 170, 80, 100);
+		g.fillOval(x, 255, 25, 65);
+		g.fillOval(x+45, 255, 25, 65);
+		g.fillOval(x-60, 170, 80, 20);
+		g.fillOval(x+50, 170, 80, 20);
 	}
 
 }
