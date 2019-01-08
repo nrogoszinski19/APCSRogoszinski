42e126eb7df1961df0796f15b8ea2608313e4062
Noa Rogoszinski <nrogoszinski19@ssdsnassau.org>
2019-01-08 14:17:01 -0500

    deleted

--- Outfits/src/Outfits.java
+++ Outfits/src/Outfits.java
@@ -5,11 +5,14 @@ import java.applet.*;
 public class Outfits extends Applet{
 	
 	public void paint(Graphics g) {
-		Person person = new Person(g, 70);
-		Shirt shirt = new Shirt(g,Color.red,70);
-		//Pants pants = new Pants(g,Color.red,70);
-		Pockets pockets = new Pockets(g, 70);
-		RippedJeans rips = new RippedJeans(g, 70);
+		Person person1 = new Person(g, 70);
+		Shirt shirt1 = new Shirt(g,Color.red,70);
+		RippedJeans rips1 = new RippedJeans(g, 70);
+		
+		Person person2 = new Person(g, 270);
+		Shirt shirt2 = new Shirt(g,Color.orange,270);
+		Pants pants2 = new Pants(g,Color.blue,270);
+		Pockets pockets2 = new Pockets(g, 270);
 
 	}
 
--- Outfits/src/Person.java
+++ Outfits/src/Person.java
@@ -4,12 +4,17 @@ public class Person {
 	
 	public Person(Graphics g, int x) {
 		g.setColor(Color.black);
+		//head
 		g.fillOval(x, 100, 70, 70);
+		//body
 		g.fillRect(x-5, 170, 80, 100);
+		//legs
 		g.fillOval(x, 255, 25, 65);
 		g.fillOval(x+45, 255, 25, 65);
+		//arms
 		g.fillOval(x-60, 170, 80, 20);
 		g.fillOval(x+50, 170, 80, 20);
+		//eyes
 		g.setColor(Color.white);
 		g.fillOval(x+10, 130, 10, 10);
 		g.fillOval(x+50, 130, 10, 10);
