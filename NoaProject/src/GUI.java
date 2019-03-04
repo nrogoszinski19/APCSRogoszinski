2529d254b445c7c4a8a33655fc41b8da020c59d7
Noa Rogoszinski <nrogoszinski19@ssdsnassau.org>
2019-03-04 18:39:18 -0500

    Car GUI

--- NoaProject/src/GUI.java
+++ NoaProject/src/GUI.java
@@ -12,6 +12,7 @@ public class GUI implements ActionListener{
 	JLabel id;
 	JLabel make;
 	JLabel model;
+	JLabel year;
 	JLabel color;
 	JLabel price;
 	JLabel cond;
@@ -21,6 +22,7 @@ public class GUI implements ActionListener{
 	JTextField i;
 	JTextField ma;
 	JTextField mo;
+	JTextField y;
 	JTextField cc;
 	JTextField p;
 	JTextField c;
@@ -28,25 +30,29 @@ public class GUI implements ActionListener{
 	JButton search;
 	JButton add;
 	JButton rem;
+	int index;
+	Inventory stock = new Inventory();
 	
 	public GUI(){
 		JPanel p1 = new JPanel();
 		p1.setLayout(new GridLayout(3,1));
 		p1.add(enter = new JLabel("Enter Car Model:"));
 		p1.add(e = new JTextField(" ", 15));
-		p1.add(rem = new JButton("Search"));
+		p1.add(search = new JButton("Search"));
 		p1.add(blank = new JLabel(" "));
 		p1.add(result = new JLabel("Result:"));
 		p1.add(r = new JTextField(" ", 15));
 		
 		JPanel p2 = new JPanel();
-		p2.setLayout(new GridLayout(8,2));
+		p2.setLayout(new GridLayout(9,2));
 		p2.add(id = new JLabel("ID: "));
 		p2.add(i = new JTextField(" ", 15));
 		p2.add(make = new JLabel("Make: "));
 		p2.add(ma = new JTextField(" ", 15));
 		p2.add(model = new JLabel("Model: "));
 		p2.add(mo = new JTextField(" "));
+		p2.add(year = new JLabel("Year: "));
+		p2.add(y = new JTextField(" "));
 		p2.add(color = new JLabel("Color: "));
 		p2.add(cc = new JTextField(" ", 15));
 		p2.add(price = new JLabel("Price: "));
@@ -71,10 +77,6 @@ public class GUI implements ActionListener{
 
 	public void actionPerformed(ActionEvent e) {
 		String str = e.getActionCommand();
-		if(str.equals("Search")) {
-			
-		}
-		
 		if(str.equals("Add Car")) {
 			
 		}
@@ -82,6 +84,10 @@ public class GUI implements ActionListener{
 		if(str.equals("Remove Car")) {
 			
 		}
+		if(str.equals("Search")) {
+			index = stock.findIndexByModel(e.getText());
+			r.setText(readFile(index));
+		}	
 	}
 	
 	public static void main(String[] args) {
