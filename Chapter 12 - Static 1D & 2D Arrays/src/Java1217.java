2f13a276942d132500c893521497225f63bea17e
Noa Rogoszinski <nrogoszinski19@ssdsnassau.org>
2019-03-11 20:00:19 -0400

    Chapter 12

---
+++ Chapter 12 - Static 1D & 2D Arrays/src/Java1217.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Java1217 {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the number of rows --> ");
+		int numRows = input.nextInt();
+		System.out.print("Enter the number of columns --> ");
+		int numCols = input.nextInt();
+		
+		int twoD[][] = new int[numRows][numCols];
+		int count;
+		for(int row = 0; row <= numRows; row++) {
+			for(int col = 0; col <=numCols; row++) {
+				
+			}
+		}
+	}
+
+}
