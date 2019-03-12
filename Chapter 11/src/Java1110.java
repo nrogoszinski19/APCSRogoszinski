226aaf66b9ab9b100e38c22721aefe780f78c079
Noa Rogoszinski <nrogoszinski19@ssdsnassau.org>
2019-03-11 20:00:12 -0400

    Chapter 11

---
+++ Chapter 11/src/Java1110.java
@@ -0,0 +1,13 @@
+
+public class Java1110 {
+
+	public static void main(String[] args) {
+		for(int table = 11; table <= 13; table++) {
+			for(int k = 1; k <= 5; k++) {
+				System.out.println(k + " * " + table + " = " + k*table);
+			}
+		}
+
+	}
+
+}
